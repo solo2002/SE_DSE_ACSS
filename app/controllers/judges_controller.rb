87c49f3cc730429cfcd778c3be52095a74603a33
Abhishek Nayyar <abhisheknayyar@Abhisheks-MacBook-Air.local>
2015-10-23 22:19:24 -0500

    add judge according to competition done

--- app/controllers/judges_controller.rb
+++ app/controllers/judges_controller.rb
@@ -11,6 +11,8 @@ class JudgesController < ApplicationController
 	end
 	def show
         	@judge = Judge.find(params[:id])
+        	@selected_competition_ids = Array.new
+
 	end
 	def judge_params
         params.require(:judge).permit(:j_name, :j_loc, :j_phone, :j_email, :j_des)

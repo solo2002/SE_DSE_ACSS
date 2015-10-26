578051ec6b9fd7448779c08e86b20ba2b44e9ca7
Abhishek Nayyar <abhisheknayyar@dhcp-10-202-133-12.tamulink.tamu.edu>
2015-10-26 18:59:32 -0500

    migration check on each page added

--- app/controllers/competitions_judges_controller.rb
+++ app/controllers/competitions_judges_controller.rb
@@ -1,11 +1,15 @@
 class CompetitionsJudgesController < ApplicationController
-	before_filter :load_competitions
-	
+	before_filter :check_authentication
 
-	def load_competitions
-
-	
+def check_authentication
+	if session[:user_type] == nil
+		flash[:notice] = 'Select one of the methods'
+		redirect_to root_path
 	end
+
+end
+
+	
 
 
 	def competitions_judges_params

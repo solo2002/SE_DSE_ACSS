d938bc3aa774fa16b4d70ec444d5c4c59e025e8c
solo2002 <jiht1999@gmail.com>
2015-11-20 00:50:31 +0000

    Score error found when adding participants to Round: fixed by Anavil in 3 min!

--- app/helpers/qualifications_helper.rb
+++ app/helpers/qualifications_helper.rb
@@ -2,9 +2,12 @@ module QualificationsHelper
 
 def calc_score(participant, round)
 	total_score = 0
-	Score.where("participant_id" => participant.id, "round_id" => round).each do |s|
-		if s.marks != nil
-			total_score += s.marks
+	if not Score.all.empty?
+		Score.where("participant_id" => participant.id, "round_id" => round).each do |s|
+			if s.marks != nil
+				total_score += s.marks
+			end
+	
 		end
 	end
 	return total_score

dfd49baa3cd855fdeb9ff1aa37965947ff0ebf37
Anavil Tripathi <anaviltripathi@gmail.com>
2015-12-01 17:55:36 -0600

    additional TDD

--- spec/controllers/scores_controller_spec.rb
+++ spec/controllers/scores_controller_spec.rb
@@ -8,32 +8,32 @@ RSpec.describe ScoresController, type: :controller do
 			@c = FactoryGirl.create(:competition)
 			@r = FactoryGirl.create(:round)
 			@ques = FactoryGirl.create(:question)
+			@p = FactoryGirl.create(:participant)
+			@s = FactoryGirl.create(:score)
+			@comments = FactoryGirl.create(:comment)
+			@qual = FactoryGirl.create(:qualification)
+
 		end
 		
-		it "should redirect to welcome page if not logged in" do
-		      get :index, :competition_id => @c.id, :round_id => @r.id
-		      response.should redirect_to root_path
-			
-		end
-
 		context "logged in as admin" do
 			it "shows an array of qualifications" do
 			session[:user_type] = 'admin'
-			get :index, :round_id => @r.id, :competition_id => @c.id
-			assigns(:questions).should eq([@ques])
-			
+			get :index, :round_id => @r.id, :participant_id => @p.id, :question_id => @ques.id 
+			assigns(:question).should eq(@ques)
 			end
 		end
 	end
     
     describe '#new' do
-    	before :each do
-		@c = FactoryGirl.create(:competition)
-		@r = FactoryGirl.create(:round)
-    	end
-        it 'should render the page for adding new question' do
+		before :each do
+			@c = FactoryGirl.create(:competition)
+			@r = FactoryGirl.create(:round)
+			@ques = FactoryGirl.create(:question)
+			@qual = FactoryGirl.create(:qualification)
+		end
+        it 'should render the page for adding new score' do
 		session[:user_type] = 'admin'
-		get :new, :round_id => @r.id, :competition_id => @c.id
+			get :new, :round_id => @r.id, :question_id => @ques.id, :competition_id => @c.id , :qualification_id => @qual.id
 		assigns(:round).should eq(@r)
         end
     end
@@ -44,13 +44,16 @@ RSpec.describe ScoresController, type: :controller do
 		before :each do
 			@c = FactoryGirl.create(:competition)
 			@r = FactoryGirl.create(:round)
+			@ques = FactoryGirl.create(:question)
+			@qual = FactoryGirl.create(:qualification)
 		end
 		
 		context "adding to first round" do
 		    it "creates a new qualification" do
 		    session[:user_type] = 'admin'
+		    session[:user_id] = 1
 		      expect{
-			post :create, :competition_id => @c.id, :round_id => @r.id, :question => FactoryGirl.attributes_for(:question)			      }.to change(Question, :count).by(1)
+			 post :create, :competition_id => @c.id, :round_id => @r.id,:question_id => @ques.id, :qualification_id => @qual.id, :scores => Array.[](1)			      }.to change(Score, :count).by(1)
 		    end
 	    
 		    it "redirects to the questions index page" do

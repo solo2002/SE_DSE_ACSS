fa3d817ab4e97a95aabef56268235f70bf13159b
Kiran Yalasangi <kirancy90@tamu.edu>
2015-12-01 17:30:54 -0600

    All validations for Qualifications

--- app/controllers/qualifications_controller.rb
+++ app/controllers/qualifications_controller.rb
@@ -55,22 +55,65 @@ def new
 end
 
 def create
+  @competition = Competition.find params[:competition_id]
 	competition = Competition.where "id" => params[:competition_id]
-	  
+	unique = Hash.new
 	if params[:first_round] != nil
 		round = Round.where "id" => params[:first_round]
 		old_qualifications = Qualification.where("round_id" => params[:first_round])
 		if !params[:arr_part]
-			flash[:warning] = "No participant to qualify"
+			
 			@round = Round.find params[:first_round]
-			@competition = Competition.find params[:competition_id]
-			redirect_to new_competition_round_qualification_path(params[:competition_id], params[:round_id]) and return
+			
+      
+      if(params[:add_part])
+         flash[:warning] = "Select at least one participant to qualify"
+        redirect_to competition_add_part_to_round_path(@competition) and return
+      else
+        flash[:warning] = "No participant to qualify"
+			  redirect_to new_competition_round_qualification_path(params[:competition_id], params[:round_id]) and return
+      end
 		else
 
 			old_qualifications.destroy_all
 			qual_params = Hash.new
 			qual_params[:round_id] = params[:first_round]
 			i=0
+      #Check if all participants who are selected have contestant number
+      params[:arr_part].each do |individual_part_id|
+        if(params[:participant_number][individual_part_id.to_s]=="")
+          flash[:warning] = "Enter Contestant Number for each selected Participants!"
+          
+         if(params[:add_part])
+            redirect_to competition_add_part_to_round_path(@competition) and return
+         else
+            redirect_to new_competition_round_qualification_path(params[:competition_id], params[:round_id]) and return
+         end
+           
+       end
+      end
+      #Check if all contestant numbers added are selected
+      params[:participant_number].keys.each do |individual_part_id|
+        if(unique.has_key? params[:participant_number][individual_part_id])
+          flash[:warning] = "Please give unique Contestant number to each participant"
+          
+          if(params[:add_part])
+             redirect_to competition_add_part_to_round_path(@competition) and return
+          else
+             redirect_to new_competition_round_qualification_path(params[:competition_id], params[:round_id]) and return
+          end
+        end
+        unique[params[:participant_number][individual_part_id]] = 1;
+        if(!params[:arr_part].include? individual_part_id.to_s)
+          flash[:warning] = "Select Participant for all the participants for whom contestant number is given!"
+          
+          if(params[:add_part])
+             redirect_to competition_add_part_to_round_path(@competition) and return
+          else
+             redirect_to new_competition_round_qualification_path(params[:competition_id], params[:round_id]) and return
+          end
+       end
+      end
 			params[:arr_part].each do |individual_part_id|
 				qual_params[:participant_number] = params[:participant_number][individual_part_id.to_s]
 				qual_params[:participant_id] = individual_part_id
@@ -78,10 +121,14 @@ def create
 				if @qualification.save
 					i=i+1
 				else
-					flash[:notice] = "random msg to idisplay message at correct place Add participant number"
-					redirect_to new_competition_round_qualification_path(params[:competition_id], params[:round_id] ) and return
+					flash[:notice] = "Participants were not sent to the round as there was some error"
+          if(params[:add_part])
+             redirect_to competition_add_part_to_round_path(@competition) and return
+          else
+             redirect_to new_competition_round_qualification_path(params[:competition_id], params[:round_id]) and return
+          end
 				end
-				end
+			end
 			flash[:notice] = "Participants successfully added to rounds"
 			redirect_to competition_round_qualifications_path(competition[0],round[0])
 		end
